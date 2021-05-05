430584047a665b428034675ac6f4138ba82d9c6c
Josiah Ebhomenye <josiah.ebhomenye@news.co.uk>
2021-05-05 20:41:10 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "GFSDK_Aftermath.h"
+#include "GFSDK_Aftermath_Defines.h"
+#include "GFSDK_Aftermath_GpuCrashDump.h"
+#include "GFSDK_Aftermath_GpuCrashDumpDecoding.h"
+
+
+
+int main(){
+
+    std::cout << "NsightAftermath\n";
+}
